829c830d92e4123b0da46cd5ef90c50533c0740a
Clément BARBASTE <barbaste.clement@gmail.com>
2017-11-14 09:56:58 +0100

    res conflit 1

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import * as $ from "jquery";
-
-import layout from './layout.html'
-
-
-export default class Layout {
-    render() {
-        $('body').html(layout)
-    }
-}
-=======
 import layout from './layout.html'
 
 export default class Layout{
@@ -17,4 +5,3 @@ export default class Layout{
     $('body').html(layout);
   }
 }
->>>>>>> 18506394f2ce78d175136032ea2e6a1fbd88f97d
