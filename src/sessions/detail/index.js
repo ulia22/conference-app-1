d17bca3ea2cf51991288b4d102c44c0be493182d
Clément BARBASTE <barbaste.clement@gmail.com>
2017-11-14 15:50:43 +0100

    Liste des pres dans session detail faite (manque css)

--- src/sessions/detail/index.js
+++ src/sessions/detail/index.js
@@ -6,25 +6,39 @@ export default class SessionDetail {
         var detail = new TalkService();
         detail.findSessionById(idSession).then(session => {
                 $("#" + idView).html(
-
-                    `
-                        <br>
+                     `
+                       <br>
                         <div class="container">
                             <div class="row">
                                 <a href="#sessions-list" class="btn btn-primary col-xs-5">retour à la liste des sessions</a>
                                 <br>
                                 <h3>${session.title}</h3>
-                                
+
                                 <div class="form-group" >
                                     ${session.desc}
                                 </div>
                             </div>
+                            <div  class="row"><div class="col" id="speakers"></div></div>
                             <div class="row">
                             <a href="#notes=${session.id}" class="btn btn-primary col-xs-5">Mes notes</a>
                         </div>
                         </div>
                     `
                 )
+                if(session.speakers){
+                  console.log(session.speakers);
+                  detail.findAllSpeakersFromSessionBySpeakersIds(session.speakers).then( speakers=>{
+                    let s = '';
+                    for (var i in speakers) {
+                     s += `
+                      <div class="row">
+                          <div class="col"><img src="../image/${speakers[i].image}"/></div>
+                          <div class="col"><a href="#speakers-list?id=${speakers[i].id}">${speakers[i].firstname} ${speakers[i].lastname}</a></div>
+                      </div>`
+                    }
+                    $('#speakers').html(s)
+                  }, this)
+                }
             }, this)
             .catch()
     }
