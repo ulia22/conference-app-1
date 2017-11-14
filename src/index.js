91afb234ed871c531ee384d78cb972e066d539ac
Pauline JACOB <pauline.lille@gmail.com>
2017-11-14 09:51:15 +0100

    recup donnees

--- src/index.js
+++ src/index.js
@@ -15,3 +15,19 @@ const talkService = new TalkService()
 const tabSpeakers = talkService.findAllSpeakers()
 
 console.log(tabSpeakers)
+
+var router = () => {
+    if (location.hash == '#speakers-list') {
+    // TODO afficher vue liste des présentateurs
+    } else if (location.hash == '#sessions-list') {
+    // TODO afficher vue liste des sessions
+    } else {
+    // TODO afficher vue par défaut
+    }
+    }
+    window.addEventListener('load', () => {
+    window.onhashchange = () => {
+    router();
+    };
+    router();
+    });
